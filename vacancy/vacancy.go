0c0e856c775e5eb15979e2d492de3907ab7d3acd
delphinus <delphinus@remora.cx>
2016-08-30 07:40:19 +0900

    Add String() method for Vacancy

--- vacancy/vacancy.go
+++ vacancy/vacancy.go
@@ -1,6 +1,9 @@
 package vacancy
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/delphinus35/moesia/util"
 )
 
@@ -8,4 +11,13 @@ import (
 type Vacancy struct {
 	Hotel string
 	Dates []*util.Time
+}
+
+func (v *Vacancy) String() (str string) {
+	var strs []string
+	for _, date := range v.Dates {
+		strs = append(strs, fmt.Sprintf("%s: %s", v.Hotel, date.MoesiaFormat()))
+	}
+	str = strings.Join(strs, "\n")
+	return
 }
