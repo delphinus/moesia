6c598ba2ff2635cbf09620530000b355dee58075
delphinus <delphinus@remora.cx>
2016-09-11 13:27:13 +0900

    Add test for mail

---
+++ mail/mail_test.go
@@ -0,0 +1,16 @@
+package mail
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/delphinus35/moesia/config"
+)
+
+func TestFailedToSend(t *testing.T) {
+	m := New(&config.Config{})
+	expected := "failed to DialAndSend"
+	if err := m.Send(""); err != nil && strings.Index(err.Error(), "failed to DialAndSend") == -1 {
+		t.Errorf("error has no expected words. expected: %v, result: %v", expected, err)
+	}
+}
